ace11c27b152717231028ecd506070d809fa6fea
Harry Singh <harsimran12326@gmail.com>
2016-01-16 19:55:43 +0530

    Add pickers to select date and time

--- app/src/main/java/io/theappx/simpletodo/activity/CreateTodoActivity.java
+++ app/src/main/java/io/theappx/simpletodo/activity/CreateTodoActivity.java
@@ -6,12 +6,22 @@ import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.theappx.simpletodo.R;
 
-public class CreateTodoActivity extends AppCompatActivity {
+public class CreateTodoActivity extends AppCompatActivity implements
+        DatePickerDialog.OnDateSetListener,
+        TimePickerDialog.OnTimeSetListener {
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
@@ -22,6 +32,11 @@ public class CreateTodoActivity extends AppCompatActivity {
     @Bind(R.id.switch_remind)
     SwitchCompat remindSwitch;
 
+    LinearLayout remindView;
+
+    DatePickerDialog mDatePickerDialog;
+    TimePickerDialog mTimePickerDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +44,20 @@ public class CreateTodoActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setUpToolbar();
+
+        Calendar lCalendar = Calendar.getInstance();
+        mDatePickerDialog = DatePickerDialog.newInstance(
+                this,
+                lCalendar.get(Calendar.YEAR),
+                lCalendar.get(Calendar.MONTH),
+                lCalendar.get(Calendar.DAY_OF_MONTH)
+        );
+        mTimePickerDialog = TimePickerDialog.newInstance(
+                this,
+                lCalendar.get(Calendar.HOUR_OF_DAY),
+                lCalendar.get(Calendar.MINUTE),
+                true
+        );
     }
 
     private void setUpToolbar() {
@@ -39,5 +68,25 @@ public class CreateTodoActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+    }
+
+    @OnClick(R.id.et_date)
+    public void selectDate() {
+        mDatePickerDialog.show(getFragmentManager(), "Choose Date");
+    }
+
+    @OnClick(R.id.et_time)
+    public void selectTime() {
+        mTimePickerDialog.show(getFragmentManager(), "Choose Time");
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
+
     }
 }
