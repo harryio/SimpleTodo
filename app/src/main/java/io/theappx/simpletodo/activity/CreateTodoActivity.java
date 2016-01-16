1bcb10452b4d3f5411d634acc79fdfd47b797852
Harry Singh <harsimran12326@gmail.com>
2016-01-16 19:01:37 +0530

    Set Toolbar title and navigation properties

--- app/src/main/java/io/theappx/simpletodo/activity/CreateTodoActivity.java
+++ app/src/main/java/io/theappx/simpletodo/activity/CreateTodoActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.EditText;
 
 import butterknife.Bind;
@@ -26,5 +27,17 @@ public class CreateTodoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_todo);
         ButterKnife.bind(this);
+
+        setUpToolbar();
+    }
+
+    private void setUpToolbar() {
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
