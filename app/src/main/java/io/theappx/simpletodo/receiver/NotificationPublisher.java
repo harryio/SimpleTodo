d77056bd3de9346b347bfab7bb7df36f254841ed
Harry Singh <me@harryio.com>
2016-02-27 00:26:49 +0530

    Fix issue of alarm not going off on some devices

--- app/src/main/java/io/theappx/simpletodo/receiver/NotificationPublisher.java
+++ app/src/main/java/io/theappx/simpletodo/receiver/NotificationPublisher.java
@@ -1,12 +1,12 @@
 package io.theappx.simpletodo.receiver;
 
 import android.app.Notification;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.WakefulBroadcastReceiver;
 
-public class NotificationPublisher extends BroadcastReceiver {
+public class NotificationPublisher extends WakefulBroadcastReceiver {
     public static final String NOTIFICATION_ID = "io.theappx.simpletodo.NOTIFICATION_ID";
     public static final String NOTIFICATION = "io.theappx.simpletodo.NOTIFICATION";
 
