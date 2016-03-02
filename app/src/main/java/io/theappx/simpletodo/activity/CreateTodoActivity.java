b5b6810cc9ce8ada66253554bdefa7f2c9f3520f
Harry Singh <me@harryio.com>
2016-03-03 01:36:32 +0530

    Change accent color and set title on RepeatIntervalDialog

--- app/src/main/java/io/theappx/simpletodo/activity/CreateTodoActivity.java
+++ app/src/main/java/io/theappx/simpletodo/activity/CreateTodoActivity.java
@@ -482,7 +482,7 @@ public class CreateTodoActivity extends AppCompatActivity implements
             @Override
             public void onClick(DialogInterface dialog, int which) {
             }
-        });
+        }).setTitle("Repeat");
 
         builder.create().show();
     }
